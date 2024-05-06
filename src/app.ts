0eace73e2e5df6876ca697c50caa613aed966d06
inoway46 <inoueyuya416@gmail.com>
2024-05-06 15:55:07 +0900

    publicディレクトリを移動

--- src/app.ts
+++ src/app.ts
@@ -34,7 +34,7 @@ const app: Application = express();
 app.set("view engine", "ejs");
 app.set("views", "./src/views");
 app.use(layouts);
-app.use("/public", express.static("src/public"));
+app.use("/public", express.static("public"));
 app.use("/", router);
 
 const textEventHandler = async (
