2073c6e72bf5c467bb1ed429d21ab90f674175f9
inoway46 <inoueyuya416@gmail.com>
2024-05-08 16:40:27 +0900

    メッセージを送信できるようにする

--- src/controllers/usersController.ts
+++ src/controllers/usersController.ts
@@ -1,7 +1,5 @@
+import { client, prisma } from "../app";
 import { Request, Response } from "express";
-import { PrismaClient } from "@prisma/client";
-
-const prisma = new PrismaClient();
 
 export default {
   index: async (req: Request, res: Response) => {
@@ -16,8 +14,44 @@ export default {
       include: { messages: true },
     });
     res.render("users/messages", {
+      userId: id,
       userName: user?.name,
       messages: user?.messages,
+      errorMessage: req.query.error_message,
     });
+  },
+
+  postUserMessage: async (req: Request, res: Response) => {
+    const { id } = req.params;
+    const { message } = req.body;
+    if (!message || message === "") {
+      const errorMessage = "メッセージを入力してください";
+      res.redirect(`/users/${id}/messages?error_message=${encodeURIComponent(errorMessage)}`);
+      return;
+    }
+    const user = await prisma.user.findUnique({
+      where: { id: Number(id) },
+    });
+    if (!user?.lineId) {
+      const errorMessage = "LINE未連携のため送信できません";
+      res.redirect(`/users/${id}/messages?error_message=${encodeURIComponent(errorMessage)}`);
+      return;
+    }
+    await client.pushMessage({
+      to: user?.lineId,
+      messages: [{ type: "text", text: message }],
+    }).catch(() => {
+      const errorMessage = "送信に失敗しました";
+      res.redirect(`/users/${id}/messages?error_message=${encodeURIComponent(errorMessage)}`);
+      return;
+    });
+    await prisma.message.create({
+      data: {
+        userId: Number(id),
+        content: message,
+        isFromUser: false,
+      },
+    });
+    res.redirect(`/users/${id}/messages`);
   },
 };
